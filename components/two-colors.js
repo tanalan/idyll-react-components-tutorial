6f0e7efe5af8dca0e7e6e7aabeb22184c25559cd
Alan Tan <taalantan@gmail.com>
2019-08-25 17:47:35 -0500

    add TwoColors

---
+++ components/two-colors.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class TwoColors extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.changePropsColor = this.changePropsColor.bind(this);
+  }
+
+  changePropsColor(color) {
+    this.props.updateProps({
+      currentColor: color
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{backgroundColor: 'green'}}
+          onClick={() => this.changePropsColor('green')}
+        >
+          Green
+        </div>
+        <div 
+          style={{backgroundColor: 'orange'}}
+          onClick={() => this.changePropsColor('orange')}
+        >
+          Orange
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TwoColors;
