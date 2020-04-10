d17419633699d48ddba700256a954a78c25387c2
Alan Tan <taalantan@gmail.com>
2020-04-09 20:12:49 -0700

    Megan's turn

--- components/budgeter.js
+++ components/budgeter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PieChart from 'react-minimal-pie-chart';
 
 class Budgeter extends React.PureComponent {
   constructor(props) {
@@ -7,8 +8,20 @@ class Budgeter extends React.PureComponent {
 
   render() {
     return (
-      <div style={{backgroundColor: 'lightGreen'}}>
-        $$$$
+      <div className="budgeter-container">
+        <div>
+          What expense?
+          What how much
+        </div>
+        <div style={{backgroundColor: 'lightGreen'}}>
+          <PieChart
+            data={[
+              { title: 'One', value: 10, color: '#E38627' },
+              { title: 'Two', value: 15, color: '#C13C37' },
+              { title: 'Three', value: 20, color: '#6A2135' },
+            ]}
+          />
+        </div>
       </div>
     );
   }
